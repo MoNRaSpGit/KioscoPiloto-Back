fe0b093ac4ede110cbce18fae3e1687fc1a8769a
MoNRaSpGit <ju4nrsuarez@gmail.com>
2024-12-31 00:16:17 -0300

    cors*

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 
 // Configuración de CORS
 const corsOptions = {
-  origin: ["http://localhost:3000", "https://monraspgit.github.io/MercadoYa"], // Permitir localhost y la URL de producción
-  methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
-  credentials: true, // Permitir cookies o encabezados de autenticación
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
 };
 app.use(cors(corsOptions));
 
