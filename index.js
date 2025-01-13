c89139c72b129b86ec7e6a9a6c21fa92573b1bcb
MoNRaSpGit <ju4nrsuarez@gmail.com>
2025-01-13 20:11:21 -0300

    cors 2

--- index.js
+++ index.js
@@ -28,9 +28,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.options("*", cors(corsOptions));
 
 
-app.use(cors(corsOptions));
+
+
 
 // Middleware para parsear JSON
 app.use(express.json({ limit: '10mb' }));
